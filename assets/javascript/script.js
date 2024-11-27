3709140ef78a64d9a00fb698f287bc9a290866ea
jonheb <74306537+JonasHeb@users.noreply.github.com>
2024-11-27 08:09:51 +0100

    sure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,10 @@
-const test = document.getElementById("test");
+const btn = document.querySelector("öffi");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+btn.addEventListener("hover", () => {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
